9e3ccc20cb8302536527a75b19889da9cfcf3066
Ordlab1 <fordlab1@MGC000000444>
2017-11-09 16:06:32 -0500

    Modifying the test runner

--- src/test/java/com/kata/bootstrap/e2e/BootstrapE2ETest.java
+++ src/test/java/com/kata/bootstrap/e2e/BootstrapE2ETest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -16,7 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ContextConfiguration(classes = BootstrapApplication.class)
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 public class BootstrapE2ETest {
 
     @Autowired
